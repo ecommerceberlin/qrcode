8991da49bfb4df256ba0f2e9279ceb8bd2acd4cb
Adam Zygadlewicz <adam@zygadlewicz.com>
2024-02-05 21:27:45 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,15 @@ const nextConfig = {
         // domains: ["www.svgrepo.com"]
       },
 
+      typescript: {
+        // !! WARN !!
+        // Dangerously allow production builds to successfully complete even if
+        // your project has type errors.
+        // !! WARN !!
+        ignoreBuildErrors: true,
+      },
+
+
 };
 
 export default nextConfig;
