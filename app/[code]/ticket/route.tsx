b4051b1978a0e123e1a8463d8b596b0cf2b68491
Adam Zygadlewicz <adam@zygadlewicz.com>
2024-02-05 21:04:08 +0100

    fixed custom font

--- app/[code]/ticket/route.tsx
+++ app/[code]/ticket/route.tsx
@@ -1,62 +1,56 @@
 
 import { ImageResponse } from "next/og"
 import { NextRequest } from "next/server"
-
+import Image from "next/image"
 // Route segment config
 export const runtime = 'edge'
  
-// Image metadata
-export const alt = 'EGA'
-export const size = {
-  width: 630,
-  height: 1200,
+
+function getAbsoluteFilename(request: NextRequest, path: string): string{
+  const {host, protocol} = request.nextUrl
+
+  return `${protocol}//${host}/_next/image?url=${encodeURIComponent(path)}&w=1200&q=75`
 }
 
-/**
- * 
- * 
 
+const getFont = async () => { 
+  const res = await fetch( new URL('@/fonts/Inter/static/Inter-SemiBold.ttf', import.meta.url) ); 
+  return await res.arrayBuffer(); 
+};
 
- */
 
 
 export async function GET(
   request: NextRequest, 
   { params }: { params: { code: string }} 
   ) {
-
-    const {host, protocol} = request.nextUrl
-
  
-
-    const cover = `${protocol}//${host}/_next/image?url=${encodeURIComponent(`/${params.code}/qrcode`)}&w=1200&q=75`
-
-
-    // const interSemiBold = fetch(
-    //   new URL('@/Inter/static/Inter-SemiBold.ttf', import.meta.url)
-    // ).then((res) => res.arrayBuffer())
-   
     return new ImageResponse(
       (
         // ImageResponse JSX element
         <div tw="flex w-full h-full flex-col justify-center bg-white">
-        <div tw="flex justify-center text-5xl mb-5">asd</div>
-        <div tw="flex justify-center"><img src={cover} width="500" height="500" alt="" /></div>
+        
+        <div tw="flex justify-center"><img tw="mb-10" src={getAbsoluteFilename(request, "/ega.png")} alt="" width="500" /></div>
+        <div tw="flex justify-center text-5xl mb-30 text-gray-700">fname lname</div>
+        <div tw="flex justify-center"><img src={getAbsoluteFilename(request, `/${params.code}/qrcode`)} width="500" height="500" alt="" /></div>
         </div>
       ),
       // ImageResponse options
       {
         // For convenience, we can re-use the exported opengraph-image
         // size config to also set the ImageResponse's width and height.
-        ...size,
-        // fonts: [
-        //   {
-        //     name: 'Inter',
-        //     data: await interSemiBold,
-        //     style: 'normal',
-        //     weight: 400,
-        //   },
-        // ],
+        
+        width: 630,
+        height: 1200,
+        
+        fonts: [
+          {
+            name: 'Inter',
+            data: await getFont(),
+            style: 'normal',
+            weight: 400,
+          },
+        ],
       }
     )
   }
